465f81ba28755cc0de32d8bb1d1eba28f6845fa3
No Nevim <cermak.mk@gmail.com>
2022-02-02 15:22:57 +0100

    vytvoreni index.html, source.js a priprava pseudocode

---
+++ source.js
@@ -0,0 +1,25 @@
+
+
+console.log("cau smradi");
+
+// vytvorim promennou "playerWins" typu Int, do ktere vlozim 0
+// vytvorim promennou "computerWins" typu Int, do ktere vlozim 0
+// vytvorim promennou "thisRoundWins" typu String, do ktere vlozim ""
+
+// vytvorim funkci "computerPlay" (bez parametru), ktera nahodne generuje volbu pocitace a vraci jeden ze stringu (rock, paper, scissors, lizzard, spock)
+// vytvorim funkci "playRound", ktera bere dva parametry - hracovu volbu (playerSelection) a volbu pocitace (computerPlay) a vrati 
+        //string s informaci, jestli hrac vyhral nebo prohral a proc (neco beats neco) + uvnitr funkce inkrementuje v zavislosti na
+        //vysledku bud promennou "playerWins" nebo "ComputerWins"
+
+// vytvorim funkci "game", ktera 5x zopakuje:
+                // zeptam se na to, co chci hrat (rock, paper, scissors, lizzard, spock) - pres prompt
+                // osetrim vstup od ostatnich zadanych moznosti (nesmi se vlozit nic krome 5 zakladnich stringu)
+                // prevedu vstup na lowercase (tim odstranim mozne problemy s case sensitive)
+                // vstup od hrace ulozim do promenne "playerSelection"
+                // spustim funkci "playRound" - se vstupy "playerSelection" a "computerSelection" a vystup vypisu do konzole (alarmu) spolecne
+                    // s prubeznym skore hrac vs pocitac   
+                //  po dobehnuti 5 her zobrazim status, kdo vyhral celkove
+
+
+
+
