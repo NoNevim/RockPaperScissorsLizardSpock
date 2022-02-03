40818de92c98bfdd3ca1ead89b9977d3d9a10335
No Nevim <cermak.mk@gmail.com>
2022-02-03 13:55:49 +0100

    completed the game, there may be some space for future improvements

--- source.js
+++ source.js
@@ -44,6 +44,7 @@ function playRound (playerSelection, computerPlay) {
         if (playerSelection === "paper" && (computerPlay === "rock" || computerPlay === "spock")) return playerWonRound(playerSelection, computerPlay);
         if (playerSelection === "paper" && (computerPlay === "scissors" || computerPlay === "lizard")) return computerWonRound(playerSelection, computerPlay);
 
+
         return `This round is a draw. The score is still ${playerWins}:${computerWins}`;
     
 }
@@ -56,13 +57,37 @@ function playRound (playerSelection, computerPlay) {
                 // spustim funkci "playRound" - se vstupy "playerSelection" a "computerSelection" a vystup vypisu do konzole (alarmu) spolecne
                     // s prubeznym skore hrac vs pocitac   
                 //  po dobehnuti 5 her zobrazim status, kdo vyhral celkove
+function game() {
+    let sayResult = "";
+    let playerSelection = "";
+    
+    //Loop:
+    for (let i = 1; i <= 5; i++) {
+        playerSelection = prompt(`Round ${i}/5. Choose: Rock, Paper, Scissors, Lizard, Spock`);
+        if (playerSelection === null) return 666;
+            
+        /* if (typeof(playerSelection) !== "string") {
+            alert("You have chosen wrong value - you have to write one of theese: Rock, Paper, Scissors, Lizard, Spock");
+        return;
+        } */
+        playerSelection = playerSelection.toLowerCase();
+        if ((playerSelection !== "rock") && (playerSelection !== "paper") && (playerSelection !== "scissors") && (playerSelection !== "lizard") && (playerSelection !== "spock")) {
+            alert("You have chosen wrong value - you have to write one of theese: Rock, Paper, Scissors, Lizard, Spock");
+            i--;
+            continue;
+        }
+        sayResult = playRound(playerSelection, computerPlay());
+        alert(sayResult);
+    }
 
-
-
-
-
-
-
+    if (playerWins > computerWins) {
+        alert(`Yayy. You are overall winner! Final score is ${playerWins}:${computerWins}`);
+    } else if (computerWins > playerWins) {
+        alert(`What a shame. Computer had more luck. Final score is ${playerWins}:${computerWins}`);
+    } else {
+        alert(`Ok, nobody wins. Shake hands with each other. It is draw: ${playerWins}:${computerWins}`)
+    }    
+}
 
 function playerWonRound(playerSelection, computerPlay) {
         playerWins += 1;
@@ -75,11 +100,17 @@ function computerWonRound(playerSelection, computerPlay) {
 }
 
 
+let keepGoing = true;
+while(keepGoing)
+    if (game() === 666 ) {
+        let playAgain =confirm("You have canceled the game, wanna play again?");
+        (playAgain) ? keepGoing = true : keepGoing = false;
+    } else {
+        let playAgain =confirm("Wanna play again?");
+        (playAgain) ? keepGoing = true : keepGoing = false;
+    }   
 
 
 
-
-
-
-console.log(playRound(computerPlay(),computerPlay()));
+//console.log(playRound(computerPlay(),computerPlay()));
 
